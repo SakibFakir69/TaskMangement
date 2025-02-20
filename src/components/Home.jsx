6bfde9aef8534f2dd57a4adf971d3cc52e4286e6
Sakib Fakir <FakirSakib22232@gmail.com>
2025-02-20 14:35:21 +0600

    modal open done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,65 +1,93 @@
-
-
-
-
-import React from 'react'
+import React, { useState } from "react";
 
 function Home() {
+  const [title, settitle] = useState("");
+  const [description, setdescription] = useState("");
+  const [category, setcategory] = useState("");
 
-    const addTaskbutton = () =>{
-        alert("add task")
-    }
+  const addTaskbutton = (event) => {
+    event.preventDefault();
+
+    alert("add task");
+    const TaskAddBYUser = {
+      Title: title,
+      Description: description,
+      Category: category,
+      Timestam: new Date().toISOString(),
+    };
+    console.log(TaskAddBYUser);
+  };
 
   return (
-    <div className='px-6 py-10'>
-        <section>
-           
-<button className="btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>open modal</button>
-<dialog id="my_modal_1" className="modal">
-  <div className="modal-box">
-    <h3 className="font-bold text-lg">Hello!</h3>
-    <p className="py-4">Press ESC key or click the button below to close</p>
-    <div className="modal-action">
-      <form method="dialog">
+    <div className="px-6 py-10">
+      <section>
+        <button
+          className="btn"
+          onClick={() => document.getElementById("my_modal_1").showModal()}
+        >
+          open modal
+        </button>
+        <dialog id="my_modal_1" className="modal">
+          <div className="modal-box">
+            <section className="flex flex-col gap-4 p-2 w-full">
+              <input
+                type="text"
+                placeholder="Enter your title"
+                maxLength={"50"}
+                onChange={(e) => settitle(e.target.value)}
+              />
 
-        <button className="btn">Close</button>
-      </form>
+              <textarea
+                className="min-h-20"
+                onChange={(e) => setdescription(e.target.value)}
+                maxLength={"2000"}
+              />
+
+              <select
+                name=""
+                id=""
+                onChange={(e) => setcategory(e.target.value)}
+              >
+                <option value={"To-Do"}>To-Do</option>
+                <option value={"In Progress"}>In Progress</option>
+                <option value={"Done"}>Done</option>
+              </select>
+            </section>
+
+            <div className="modal-action">
+              <form method="dialog" onSubmit={addTaskbutton}>
+                <button
+                  className="btn"
+                  onClick={() => document.getElementById("my_modal_1").close()}
+                >
+                  Close
+                </button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      </section>
+
+      {/* 3 section  */}
+
+      <section className="grid w-full lg:grid-cols-3 border gap-4 md:grid-cols-2 py-6">
+        <div className="border">
+          <h1>Todo</h1>
+          <div>start</div>
+        </div>
+
+        <div className="border">
+          <h2>Processing</h2>
+          <div>start</div>
+        </div>
+        <div className="border">
+          <h2>Done</h2>
+
+          <div>start</div>
+        </div>
+      </section>
     </div>
-  </div>
-</dialog>
-        </section>
-
-
-        {/* 3 section  */}
-
-        <section className='grid w-full lg:grid-cols-3 border gap-4 md:grid-cols-2 py-6' >
-
-            <div className='border'>
-                <h1>Todo</h1>
-                <div>
-                    start
-                </div>
-            </div>
-
-            <div className='border'>
-                <h2>Processing</h2>
-                <div>
-                    start
-                </div>
-            </div>
-            <div className='border'>
-                <h2>Done</h2>
-
-                <div>
-                    start
-                </div>
-            </div>
-        </section>
-
-        
-        
-    </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
